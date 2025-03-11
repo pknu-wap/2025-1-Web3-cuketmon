6a2a672d4adaf835d17a4d17eb0904706829a558
Chan Yi <2chan204@gmail.com>
2025-03-11 14:30:24 +0900

    [feat] 메뉴바- 화면 이동

--- cuketmon/src/menubar/Menubar.js
+++ cuketmon/src/menubar/Menubar.js
@@ -1,13 +1,25 @@
-/*메뉴바*/
-import './menubar.css'
+import { Link } from 'react-router-dom';
+import './menubar.css';
 
 const MenuBar = () => {
   return (
     <div className="menubar-items">
-      <button className="makePage"><img src='./egg.png'/>커켓몬 만들기</button>
-      <button className="battle"><img src='./battleicon.png'/>전투</button>
-      <button className="ranking"><img src='./rankingicon.png'/>랭킹</button>
-      <button className="mypage"><img src='./mypageicon.png'/>mypage</button>
+      <button className="makePage">
+        <img src='./egg.png' alt="egg" />
+        <Link to="/Make">커켓몬 만들기</Link>
+      </button>
+      <button className="battle">
+        <img src='./battleicon.png' alt="battle" />
+        <Link to="/battle">전투</Link>
+      </button>
+      <button className="ranking">
+        <img src='./rankingicon.png' alt="ranking" />
+        <Link to="/ranking">랭킹</Link>
+      </button>
+      <button className="mypage">
+        <img src='./mypageicon.png' alt="mypage" />
+        <Link to="/mypage">mypage</Link>
+      </button>
     </div>
   );
 };
