884a674f5dac8f8708274820acdbd7575ef3e400
jso <c50691464@gmail.com>
2025-03-15 19:41:22 +0900

    [feat] 전투화면- 배경추가
    -전투 배경화면 적용

--- cuketmon/src/battle.js
+++ cuketmon/src/battle.js
@@ -1,15 +1,46 @@
 /*랭킹 화면 */
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import MenuBar from "./menubar/Menubar.js";
 import './battle.css';
 
-function Make() {
+function Battle() {
+  const [myCuketmon, setMyCuketmon] = useState("");
+  const [enemyCuketmon, setEnemyCuketmon] = useState("");
+  const [hpBar, setHpBar] = useState(100);
+  const [enemyHpBar, setEnemyHpBar] = useState(100);
+  const techs = [
+    { id: 1, name: '기술 1', description: '기술1' },
+    { id: 2, name: '기술 2', description: '기술2' },
+    { id: 3, name: '기술 3', description: '기술3' },
+    { id: 4, name: '기술 4', description: '기술4' },
+  ];
+
+  const [selectedTech, setselectedTech] = useState(null);
+  const [description, setDescription] = useState('');
+
+  
   return (
-    <div>
-      <h2>배틀 페이지로 이동했습니다.</h2>
-      <MenuBar />
+    <div className= 'Battle'>
+        <img src="/battlebg.png" alt="배경" className="background"/>
+        <div className='myCuketmon'>
+         <img src='cuketmonex.png' alt='myCuketmon'/></div>
+         <div className='enemyCuketmon'>
+         <img src='cuketmonex.png' alt='enemyCuketmon'/></div>
+        
+
+
+       <div className='hpbar'>
+         <div className='myHpBar'></div>
+         <div className='enemyHpBar'></div>
+       </div>
+
+      <div className='selectSkill'>
+
+      </div>
+
     </div>
+   
   );
 }
 
-export default Make;
+export default Battle;
