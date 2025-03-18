39d18efc8b03a9318375f87106b8578884f58ff5
Chan Yi <2chan204@gmail.com>
2025-03-18 17:46:08 +0900

    [feat] 생성결과화면- 배경화면 적용, 기초 UI/UX 작업
    -알 gif 적용
    -멘트 박스 적용
    -배경화면 적용

--- cuketmon/src/MakeResult.js
+++ cuketmon/src/MakeResult.js
@@ -4,9 +4,11 @@ import "./MakeResult.css";
 
 function MakeResult() {
   return (
-    <div>
-      <h2>포켓몬 생성이 완료되었습니다!</h2>
-      <p>입력하신 정보는 성공적으로 제출되었습니다. 결과 페이지를 확인해주세요!</p>
+    <div className="ResultPage">
+      <img id='egg'src='/MakeResultPage/MovingEgg.gif'/>
+      <div className="chatbox">
+        <p id='ment'>어라...?</p>
+      </div>
     </div>
   );
 }
