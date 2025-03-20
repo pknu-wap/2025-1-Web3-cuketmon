baab4b26852bfccd65e2efecd8bc6e9458b9ba70
Chan Yi <2chan204@gmail.com>
2025-03-20 16:15:42 +0900

    [fix] 마이페이지- 이미지 로딩 오류 해결
    -새로고침 시, 이미지 로딩안되는 오류 해결

--- cuketmon/src/menubar/Menubar.js
+++ cuketmon/src/menubar/Menubar.js
@@ -5,19 +5,19 @@ const MenuBar = () => {
   return (
     <div className="menubar-items">
       <button className="makePage">
-        <img src='./egg.png' alt="egg" />
+        <img src='/egg.png' alt="egg" />
         <Link to="/Make">커켓몬 만들기</Link>
       </button>
       <button className="battle">
-        <img src='./battleicon.png' alt="battle" />
+        <img src='/battleicon.png' alt="battle" />
         <Link to="/battle">전투</Link>
       </button>
       <button className="ranking">
-        <img src='./rankingicon.png' alt="ranking" />
+        <img src='/rankingicon.png' alt="ranking" />
         <Link to="/ranking">랭킹</Link>
       </button>
       <button className="mypage">
-        <img src='./mypageicon.png' alt="mypage" />
+        <img src='/mypageicon.png' alt="mypage" />
         <Link to="/mypage">mypage</Link>
       </button>
     </div>
