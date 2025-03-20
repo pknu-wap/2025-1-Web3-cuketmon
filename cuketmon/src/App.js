f1cdc761990ff7f24d94778e441f78a7f01503d1
Chan Yi <2chan204@gmail.com>
2025-03-20 19:33:19 +0900

    [fix] 클래스 이름 변경 및 파일 정리
    -네이밍 컨벤션(카멜 케이스) 적용
    -파일 폴더별 정리 완료

--- cuketmon/src/App.js
+++ cuketmon/src/App.js
@@ -5,10 +5,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <img src='/logo.png' id='weblogo' alt="웹 로고"/>
-      <Link to='/make' className="button-container">
-          <span className="button-text">카카오 로그인으로 시작하기</span>
-          <img src='/button.png' alt="카카오 로그인 버튼" className="button-image"/>
+      <img src='/logo.png' id='webLogo' alt="웹 로고"/>
+      <Link to='/make' className="buttonContainer">
+          <span className="buttonText">카카오 로그인으로 시작하기</span>
+          <img src='/button.png' alt="카카오 로그인 버튼" className="buttonImage"/>
       </Link>
     </div>
   );
