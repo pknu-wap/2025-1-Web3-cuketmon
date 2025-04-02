49aec4d3e340aa90c1b07dac0c6281babf6e09f1
kangrae <kangrae0501@gmail.com>
2025-04-02 22:59:12 +0900

    [feat] 배틀에 사용할 클래스 구현
    - 데이터베이스에 값이 바뀌는 것과 캐시 메모리를 고려하여 전투용 객체 생성

--- BE/src/main/java/cuketmon/battle/dto/BattleDTO.java
+++ BE/src/main/java/cuketmon/battle/dto/BattleDTO.java
@@ -1,4 +1,54 @@
 package cuketmon.battle.dto;
 
+import cuketmon.type.Type;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public class BattleDTO {
+
+    @Getter
+    @AllArgsConstructor
+    public static class Team {
+        private Integer battleId;
+        private String trainerName;
+        private Monster monster;
+        private Boolean turn;
+    }
+
+    @Getter
+    @AllArgsConstructor
+    public static class Monster {
+        private String name;
+        private String image;
+        private Integer affinity;
+
+        private Integer hp;
+        private Integer speed;
+        private Integer attack;
+        private Integer defence;
+        private Integer specialAttack;
+        private Integer specialDefence;
+
+        private Type type1;
+        private Type type2;
+
+        private Skill skill1;
+        private Skill skill2;
+        private Skill skill3;
+        private Skill skill4;
+    }
+
+    @Getter
+    @AllArgsConstructor
+    public static class Skill {
+        private Type type;
+        private String damageClass;
+        private Integer accuracy;
+        private String name;
+        private Integer power;
+        private Integer pp;
+    }
+
 }
--- BE/src/main/java/cuketmon/battle/dto/SkillRequest.java
+++ BE/src/main/java/cuketmon/battle/dto/SkillRequest.java
@@ -1,5 +1,10 @@
 package cuketmon.battle.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public class SkillRequest {
 
     Integer skillId;
