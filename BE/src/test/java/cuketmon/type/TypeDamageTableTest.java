bf3d81369cc54f3c65e0eed20ffc9976bace3755
kangrae <kangrae0501@gmail.com>
2025-04-07 02:45:44 +0900

    [fix] 테스트 코드 유지보수
    - static이된 TypeDamageTableTest을 받을 수 있도록 변경

--- BE/src/test/java/cuketmon/type/TypeDamageTableTest.java
+++ BE/src/test/java/cuketmon/type/TypeDamageTableTest.java
@@ -1,5 +1,6 @@
 package cuketmon.type;
 
+import static cuketmon.type.TypeDamageTable.getDamageMultiplier;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
@@ -8,12 +9,11 @@ class TypeDamageTableTest {
 
     @Test
     void 타입별_데미지_테스트() {
-        TypeDamageTable typeDamageTable = new TypeDamageTable();
-        assertEquals(2.0, typeDamageTable.getDamageMultiplier(Type.FIRE, Type.GRASS));
-        assertEquals(0.5, typeDamageTable.getDamageMultiplier(Type.GRASS, Type.FIRE));
-        assertEquals(2.0, typeDamageTable.getDamageMultiplier(Type.FAIRY, Type.DRAGON));
-        assertEquals(1.0, typeDamageTable.getDamageMultiplier(Type.NORMAL, Type.NORMAL));
-        assertEquals(0.0, typeDamageTable.getDamageMultiplier(Type.GHOST, Type.NORMAL));
+        assertEquals(2.0, getDamageMultiplier(Type.FIRE, Type.GRASS));
+        assertEquals(0.5, getDamageMultiplier(Type.GRASS, Type.FIRE));
+        assertEquals(2.0, getDamageMultiplier(Type.FAIRY, Type.DRAGON));
+        assertEquals(1.0, getDamageMultiplier(Type.NORMAL, Type.NORMAL));
+        assertEquals(0.0, getDamageMultiplier(Type.GHOST, Type.NORMAL));
     }
 
 }
