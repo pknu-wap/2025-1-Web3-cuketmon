f1163e9201a164a4faba2a2f557e356a20f908e1
kangrae <kangrae0501@gmail.com>
2025-04-18 23:02:46 +0900

    [feat] 사용자 인증 상태 유지 기능 구현
    - 로그인을 하고, 그 뒤에는 JWT 토큰으로 API 요청을 할 수 있도록 구현
    - 서비스 로직에서 로그인한 사용자 이름만으로 처리가능

--- BE/src/main/java/cuketmon/SecurityConfig.java
+++ BE/src/main/java/cuketmon/SecurityConfig.java
@@ -1,6 +1,7 @@
 package cuketmon;
 
 import cuketmon.oauth.service.CustomOAuth2UserService;
+import cuketmon.oauth.service.JwtAuthenticationFilter;
 import cuketmon.oauth.service.OAuth2SuccessHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
@@ -9,6 +10,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @RequiredArgsConstructor
@@ -17,6 +19,8 @@ public class SecurityConfig {
 
     private final CustomOAuth2UserService customOAuth2UserService;
     private final OAuth2SuccessHandler oAuth2SuccessHandler;
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -32,7 +36,9 @@ public class SecurityConfig {
                     oauth2
                             .userInfoEndpoint(userInfo -> userInfo.userService(customOAuth2UserService))
                             .successHandler(oAuth2SuccessHandler);
-                });
+                })
+                // JWT 필터를 UsernamePasswordAuthenticationFilter 앞에 추가
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
