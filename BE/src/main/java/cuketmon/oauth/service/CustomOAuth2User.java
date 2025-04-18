13c2a3a5e19545826e9355ad031fbd8a9ff2dd72
kangrae <kangrae0501@gmail.com>
2025-04-18 22:38:59 +0900

    [feat] OAuth 기능 구현
    - OAuth2User 등에서 기본으로 제공되는 기능을 확장 or 구현하여 사용
    - 커켓몬 프로젝트에서 User로 취급하는 Trainer는 name이 id임으로, kakao email의 '@' 앞부분을 name으로 설정

---
+++ BE/src/main/java/cuketmon/oauth/service/CustomOAuth2User.java
@@ -0,0 +1,32 @@
+package cuketmon.oauth.service;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Map;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+@Getter
+public class CustomOAuth2User implements OAuth2User {
+
+    private final String name;
+    private final Map<String, Object> attributes;
+
+    public CustomOAuth2User(String name, Map<String, Object> attributes) {
+        this.name = name;
+        this.attributes = attributes;
+    }
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return attributes;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singleton(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+}
--- BE/src/main/java/cuketmon/trainer/entity/Trainer.java
+++ BE/src/main/java/cuketmon/trainer/entity/Trainer.java
@@ -4,12 +4,14 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
