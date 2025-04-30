65dc7776b36b050880c8938a15ed7445d95af227
Chan Yi <2chan204@gmail.com>
2025-04-30 18:30:45 +0900

    [feat] 생성페이지- 글자 수 조정 및 버튼 추가
    - 커켓몬 특징 프롬프트 최대 글자 수 35자로 제한
    - 기존 구현은 직관적이 못 해서 아쉬웠음. 이를 해결하고자 데려가기 버튼 추가
    - 재부화 버튼을 추가해 다시 커켓몬을 생성할 수 있도록함

--- cuketmon/src/NamePage/NamePage.js
+++ cuketmon/src/NamePage/NamePage.js
@@ -6,7 +6,7 @@ function NamePage() {
   const [name, setName] = useState("");
   const [pokemonImage, setPokemonImage] = useState("/Menubar/egg.png"); // 임시 이미지 백엔드 연동시 바꿀 예정
   const navigate = useNavigate();
-  const maxLength = 12; 
+  const maxLength = 12;
 
   useEffect(() => {
     if (name) {
@@ -25,13 +25,14 @@ function NamePage() {
     if (e.target.value.length <= maxLength) {
       setName(e.target.value);
     }
-
   };
 
-  const handleBlur = () => {
+  const handleGoToMypage = () => {
     if (name.trim().length > 0) {
-      alert("커켓몬이 생성되었습니다.")
-      navigate("/mypage"); //마이페이지로 이동해 커켓몬이 제대로 생성되었는지 확인할 수 있게!
+      alert("커켓몬이 생성되었습니다.");
+      navigate("/mypage"); 
+    } else {
+      alert("이름을 입력해주세요.");
     }
   };
 
@@ -39,23 +40,36 @@ function NamePage() {
     <div className="namePage">
       <div className="nameInputBox">
         <p>Your name?</p>
-        {pokemonImage && <img src={pokemonImage} alt="커켓몬 이미지" className="cukemonImage" />}
+        {pokemonImage && (
+          <img
+            src={pokemonImage}
+            alt="커켓몬 이미지"
+            className="cukemonImage"
+          />
+        )}
 
         <div className="nameInput">
-          ▶<input 
-            type="text" 
+          ▶
+          <input
+            type="text"
             value={name}
-            onChange={namingCukemon} 
-            placeholder="커켓몬 이름을 입력해주세요." 
-            maxLength={maxLength} 
-            onBlur={handleBlur}  
+            onChange={namingCukemon}
+            placeholder="커켓몬 이름을 입력해주세요."
+            maxLength={maxLength}
           />
-          <p id='ramainWord'>{name.length}/{maxLength}자</p>
+          <div id="remainWord">{name.length}/{maxLength}자</div>
         </div>
 
+        <div className='choiceButtons'>
+        <img src="/button.png" id="remake"  />
+        <img src="/button.png" id="bringToMypage" onClick={handleGoToMypage} />
+        <span id="buttonText1">재부화</span>
+        <span id="buttonText2">데려가기</span>
+        </div>
       </div>
     </div>
   );
 }
 
 export default NamePage;
+
