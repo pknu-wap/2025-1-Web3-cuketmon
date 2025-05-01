20923f97ca068685524b64fb316421f19a8818aa
kangrae <kangrae0501@gmail.com>
2025-05-02 03:02:01 +0900

    [release] 배포
    - CORS 명시적으로 해결

--- BE/src/main/java/cuketmon/monster/controller/MonsterController.java
+++ BE/src/main/java/cuketmon/monster/controller/MonsterController.java
@@ -6,6 +6,7 @@ import cuketmon.monster.service.MonsterService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "https://frolicking-gnome-f1b1ad.netlify.app")
 @Validated
 @RestController
 @RequestMapping("/monster")
