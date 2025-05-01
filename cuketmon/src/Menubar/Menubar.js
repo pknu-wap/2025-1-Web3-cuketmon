eb4aa6f6f326bcbf0630e1d99cff1ef2398ab35a
Chan Yi <2chan204@gmail.com>
2025-05-01 20:05:08 +0900

    [feat] 로그인화면- 토큰 저장 구현
    - 토큰을 저장할 수 있게함

--- cuketmon/src/Menubar/Menubar.js
+++ cuketmon/src/Menubar/Menubar.js
@@ -1,29 +1,33 @@
-import { useNavigate } from 'react-router-dom';
+import { useAuth } from '../AuthContext';
 import './Menubar.css';
 
 const MenuBar = () => {
-  const navigate = useNavigate(); 
+  const { token } = useAuth(); 
 
   const handleNavigation = (path) => {
-    navigate(path);
+    const url = new URL(window.location.origin + path);
+    if (token) {
+      url.searchParams.set('token', token);
+    }
+    window.location.href = url.toString();
   };
 
   return (
     <div className="menubarItems">
-      <button className="makePageLink" onClick={() => handleNavigation('/Make')}>
-        <img src='/Menubar/egg.png' alt="egg" />
+      <button className="makePageLink" onClick={() => handleNavigation('/make')}>
+        <img src="/Menubar/egg.png" alt="egg" />
         커켓몬 만들기
       </button>
       <button className="battleLink" onClick={() => handleNavigation('/battle')}>
-        <img src='/Menubar/battleicon.png' alt="battle" />
+        <img src="/Menubar/battleicon.png" alt="battle" />
         전투
       </button>
       <button className="rankingLink" onClick={() => handleNavigation('/ranking')}>
-        <img src='/Menubar/rankingicon.png' alt="ranking" />
+        <img src="/Menubar/rankingicon.png" alt="ranking" />
         랭킹
       </button>
       <button className="myPageLink" onClick={() => handleNavigation('/mypage')}>
-        <img src='/Menubar/mypageicon.png' alt="myPage" />
+        <img src="/Menubar/mypageicon.png" alt="myPage" />
         마이페이지
       </button>
     </div>
