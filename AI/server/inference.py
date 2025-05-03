e5dfc853d9608d1e1bdff8ef069cf4570a8778d5
yspark <parkite576@gmail.com>
2025-05-03 13:05:06 +0900

    [refactor] inference 전에 미리 모델 로드
    previous code: model had been loaded every inference step
    now: model pre-loaded and inference only.

--- AI/server/inference.py
+++ AI/server/inference.py
@@ -8,15 +8,20 @@ from PIL import Image
 from pathlib import Path
 from sqlalchemy.orm import Session
 
-def model_inference(monster_id: int, monster_description: str):
-    print("runing")
-    checkpoint = CHECKPOINT
-    pipeline = AutoPipelineForText2Image.from_pretrained('stable-diffusion-v1-5/stable-diffusion-v1-5', torch_dtype=torch.float16).to('cuda')
-    pipeline.load_lora_weights(f'{MODEL_PATH}/checkpoint-{checkpoint}')
-    print("model loaded")
 
-    image = pipeline(f"a poketmon, {monster_description}, no background").images[0]
+print("Loading model...")
+pipeline = AutoPipelineForText2Image.from_pretrained(
+    'stable-diffusion-v1-5/stable-diffusion-v1-5', 
+    torch_dtype=torch.float16
+).to('cuda')
+pipeline.load_lora_weights(f'{MODEL_PATH}/checkpoint-{CHECKPOINT}')
+print("Model loaded.")
+
+def model_inference(monster_id: int, monster_description: str):
+    prompt = f"a poketmon, {monster_description}, no background"
+    image = pipeline(prompt).images[0]
     return image
+
 
 def save_image(image: Image.Image, monster_id: int, monster, db: Session):
     resized_image = image.resize((81, 81))
