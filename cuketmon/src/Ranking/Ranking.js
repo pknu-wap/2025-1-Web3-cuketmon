003e8fbfa9536c61c59d23e57a6317ef909da20b
Chan Yi <2chan204@gmail.com>
2025-05-07 01:35:40 +0900

    [fix] 랭킹화면- 백엔드 연동 코드 수정 및 css 수정
    - 백엔드 연동 코드 엔드포인트를 수정함
    - 백엔드 연동 코드 trainerName 삭제
    - 커켓몬 이미지를 크기 증가
    - Ranking.css에서 필요없는 패딩을 삭제함

--- cuketmon/src/Ranking/Ranking.js
+++ cuketmon/src/Ranking/Ranking.js
@@ -3,53 +3,71 @@ import MenuBar from "../Menubar/Menubar.js";
 import './Ranking.css';
 
 function Ranking() {
-  const [Myrank, setMyrank] = useState(0);
-  const [battleCount, setBattleCount] = useState(0);
+  const [rank, setRank] = useState(0); 
+  const [trainerName, setTrainerName] = useState(''); 
   const [winCount, setWinCount] = useState(0);
-  const [imageUrl, setImageUrl] = useState('/Menubar/egg.webp');
+  const [loseCount, setLoseCount] = useState(0); 
+  const [battleCount, setBattleCount] = useState(0);
+  const [imageUrl, setImageUrl] = useState('/Menubar/egg.webp'); // 전투 API 사용할 수 있을지 확인 필요
+  const API_URL = process.env.REACT_APP_API_URL;
+  const token = localStorage.getItem('token'); 
 
-
-  const trainerName = "xami"; //임시로 xami로 둠. 나중에 카카오 로그인 연동 후 각 사용자 이름을 받아오게 만들것!!
   useEffect(() => {
-    fetch(`/${trainerName}/rank`) 
-      .then(response => response.json())
-      .then(data => {
-        setMyrank(data.rank);
-        setBattleCount(data.battleCount);
-        setWinCount(data.winCount);
-        setImageUrl(data.imageUrl); 
-      })
-      .catch(error => console.error('Error fetching ranking data:', error));
-  }, []);
+    const fetchRankingData = async () => {
+      try {
+        const response = await fetch(`${API_URL}/api/ranking`, {
+          method: 'GET', 
+          headers: {
+            'Authorization': `Bearer ${token}`
+          }
+        });
+        const data = await response.json();
+
+        console.log(data)
+        setRank(data.rank);
+        setTrainerName(data.trainerName);
+        setWinCount(data.win);
+        setLoseCount(data.lose);
+        setBattleCount(data.allBattles);
+      } catch (error) {
+        console.error('Error fetching ranking data:', error);
+      }
+    };
+    fetchRankingData();
+  }, [API_URL, token]);
 
   return (
     <div className='ranking'>
-    <div className='rankingBoard'>
-      <img src={imageUrl} alt='최근 사용 커켓몬' className="pokemonImage" />
-  <table className="historyBoard">
-    <thead>
-      <tr>
-        <th>Standing</th>
-        <th>No. {Myrank}</th>
-      </tr>
-    </thead>
-    <tbody>
-  <tr>
-    <td className='mark1'>No. of Battles</td>
-    <td>{battleCount}</td>
-  </tr>
-  <tr>
-    <td className='mark2'>Wins</td>
-    <td>{winCount}</td>
-  </tr>
-  <tr>
-    <td className='mark3'>Losses</td>
-    <td>{battleCount - winCount}</td>
-  </tr>
-</tbody>
-  </table>
-    </div>      
-    <MenuBar />
+      <div className='rankingBoard'>
+        <img src={imageUrl} alt='최근 사용 커켓몬' className="pokemonImage" />
+        <table className="historyBoard">
+          <thead>
+            <tr>
+              <th>Standing</th>
+              <th>No. {rank}</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td className='mark1'>Trainer Name</td>
+              <td>{trainerName}</td>
+            </tr>
+            <tr>
+              <td className='mark1'>No. of Battles</td>
+              <td>{battleCount}</td>
+            </tr>
+            <tr>
+              <td className='mark2'>Wins</td>
+              <td>{winCount}</td>
+            </tr>
+            <tr>
+              <td className='mark3'>Losses</td>
+              <td>{loseCount}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>      
+      <MenuBar />
     </div>
   );
 }
