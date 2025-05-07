133cf829c14e13bfb9149cd09ff5f03638903e14
kangrae <kangrae0501@gmail.com>
2025-05-07 11:48:06 +0900

    [fix] 기존 테스트코드 수정
    - 더미 데이터를 만들때 커켓몬의 주인을 지정해줌으로 오류 해결

--- BE/src/test/java/cuketmon/monster/service/MonsterServiceTest.java
+++ BE/src/test/java/cuketmon/monster/service/MonsterServiceTest.java
@@ -1,5 +1,6 @@
 package cuketmon.monster.service;
 
+import static cuketmon.constant.TestConfig.TRAINER1;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import cuketmon.config.TestDummyDataConfig;
@@ -32,23 +33,23 @@ class MonsterServiceTest {
 
     @Test
     void 먹이가_올바르게_차감되어_DB에_저장되고_조회된다() {
-        Trainer trainer = trainerRepository.findById("kng").get();
+        Trainer trainer = trainerRepository.findById(TRAINER1).get();
         int prevFeed = trainer.getFeed().getCount();
 
-        monsterService.feed("kng", 1);
+        monsterService.feed(TRAINER1, 1);
 
-        Trainer updatedTrainer = trainerRepository.findById("kng").get();
+        Trainer updatedTrainer = trainerRepository.findById(TRAINER1).get();
         assertEquals(prevFeed - 1, updatedTrainer.getFeed().getCount());
     }
 
     @Test
     void 장난감이_올바르게_차감되어_DB에_저장되고_조회된다() {
-        Trainer trainer = trainerRepository.findById("kng").get();
+        Trainer trainer = trainerRepository.findById(TRAINER1).get();
         int prevToy = trainer.getToy().getCount();
 
-        monsterService.play("kng", 1);
+        monsterService.play(TRAINER1, 1);
 
-        Trainer updatedTrainer = trainerRepository.findById("kng").get();
+        Trainer updatedTrainer = trainerRepository.findById(TRAINER1).get();
         assertEquals(prevToy - 1, updatedTrainer.getToy().getCount());
     }
 
@@ -57,7 +58,7 @@ class MonsterServiceTest {
         Monster monster = monsterRepository.findById(1).get();
         int prevAffinity = monster.getAffinity().getCount();
 
-        monsterService.play("kng", 1);
+        monsterService.play(TRAINER1, 1);
 
         Monster updatedMonster = monsterRepository.findById(1).get();
         assertEquals(prevAffinity + 1, updatedMonster.getAffinity().getCount());
