6a32c0419868b5012ec241bf26095cafad04a0eb
Chan Yi <2chan204@gmail.com>
2025-05-08 21:12:59 +0900

    [refactor] 마이페이지- 코드 구조 개선
    - Type.js에 타입에 관한 정보를 기록해 둠.
    - 해당 파일을 import 해 코드 구조 개선
    - selectBox가 타입별 색깔로 텍스트가 출력되도록 함.

---
+++ cuketmon/src/Type.js
@@ -0,0 +1,73 @@
+const typeData = {
+    fire: {
+      korean: '불꽃',
+      color: '#EF7374'
+    },
+    grass: {
+      korean: '풀',
+      color: '#82C274'
+    },
+    electric: {
+      korean: '전기',
+      color: '#FCD659'
+    },
+    psychic: {
+      korean: '에스퍼',
+      color: '#F584A8'
+    },
+    ice: {
+      korean: '얼음',
+      color: '#81DF77'
+    },
+    dragon: {
+      korean: '드래곤',
+      color: '#8D98EC'
+    },
+    dark: {
+      korean: '악',
+      color: '#998B8C'
+    },
+    fairy: {
+      korean: '페어리',
+      color: '#F5A2F5'
+    },
+    fighting: {
+      korean: '격투',
+      color: '#FFAC59'
+    },
+    flying: {
+      korean: '비행',
+      color: '#ADD2F5'
+    },
+    ghost: {
+      korean: '고스트',
+      color: '#A284A2'
+    },
+    ground: {
+      korean: '땅',
+      color: '#B88E6F'
+    },
+    poison: {
+      korean: '독',
+      color: '#B884DD'
+    },
+    rock: {
+      korean: '바위',
+      color: '#CBC7AD'
+    },
+    steel: {
+      korean: '강철',
+      color: '#98C2D1'
+    },
+    bug: {
+      korean: '벌레',
+      color: '#B8C26A'
+    },
+    normal: {
+      korean: '노말',
+      color: '#C1C2C1'
+    }
+  };
+  
+  export default typeData;
+  
