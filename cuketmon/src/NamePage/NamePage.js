1f7d3a9f5d38c657efa76af26d311a66b31fb3ee
이찬이 <158177151+chanxami@users.noreply.github.com>
2025-05-09 18:17:20 +0900

    Revert "[fix] 이름지정화면- 버튼 클릭 오류 해결"

--- cuketmon/src/NamePage/NamePage.js
+++ cuketmon/src/NamePage/NamePage.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useRef,useState, useEffect } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 import { useAuth } from "../AuthContext";
 import "./NamePage.css";
@@ -17,17 +17,27 @@ function NamePage() {
   const cukemonResultImage = location.state?.image; 
 
 //뒤로가기 막기 (5/9)
+ const backBlockRef = useRef(false); 
 useEffect(() => {
-  const handlePopState = (event) => {
-    alert("커켓몬을 두고 떠나지마요 ㅠㅠㅠ");
-    window.history.pushState(null, "", window.location.href);
+  const preventGoBack = (event) => {
+    if (event.type === "popstate") {
+      if (backBlockRef.current) return;
+      backBlockRef.current = true;
+
+      alert("커켓몬을 두고 떠나지마요 ㅠㅠㅠ");
+      window.history.go(1);
+
+      setTimeout(() => {
+        backBlockRef.current = false;
+      }, 500);
+    }
   };
 
   window.history.pushState(null, "", window.location.href);
-  window.addEventListener("popstate", handlePopState);
+  window.addEventListener("popstate", preventGoBack);
 
   return () => {
-    window.removeEventListener("popstate", handlePopState);
+    window.removeEventListener("popstate", preventGoBack);
   };
 }, []);
 
