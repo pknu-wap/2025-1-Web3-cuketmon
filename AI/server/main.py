845fe25a4e1c53a8406495cb54da90926042dddd
yspark <parkite576@gmail.com>
2025-05-09 15:23:12 +0900

    [feat] model parallel serving
    - model serving on bentoml
    - utilizing 2 gpu
    - num_inferece_steps 50 -> 25 (to reduce infence time)

--- AI/server/main.py
+++ AI/server/main.py
@@ -1,13 +1,12 @@
-from fastapi import FastAPI, Depends, HTTPException, BackgroundTasks
-from sqlalchemy.orm import Session
 import time
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
 from threading import Thread
-from . import database, models, crud, schemas
+from . import database, crud, schemas
 from .inference import model_inference
-import time
-from sqlalchemy.orm import Session
 from .crud import get_all_data, update_image_to_GCS, delete_prompt_entries
 from .utils import save_image
+
 app = FastAPI()
 
 @app.on_event("startup")
@@ -20,29 +19,34 @@ def shutdown():
     database.disconnect_db()
 
 def process_images():
+    batch_size = 8
+
     while True:
         db = database.SessionLocal()
         try:
-            null_images = get_all_data(db)
-            if null_images:
-                batch_size = 4
-                for i in range(0, len(null_images), batch_size):
-                    batch = null_images[i:i+batch_size]
-                    for monster in batch:
-                        monster_id = monster.id
-                        prompt = monster.description
+            # DB에서 최대 8개 가져오기
+            monsters = get_all_data(db)[:batch_size]
+            if monsters:
+                prompts = [m.description for m in monsters]
+                inference_results = model_inference(prompts)
 
-                        image = model_inference(prompt)
-                        gcs_url = save_image(image, monster_id)
-                        update_image_to_GCS(monster_id, gcs_url, db)
-                        delete_prompt_entries(monster_id, db)
+                for monster, (prompt, img_bytes) in zip(monsters, inference_results):
+                    if img_bytes:
+                        gcs_url = save_image(img_bytes, monster.id)
+                        update_image_to_GCS(monster.id, gcs_url, db)
+                        delete_prompt_entries(monster.id, db)
                         db.commit()
+                    else:
+                        print(f"[{monster.id}] Failed to generate image for prompt: {prompt}")
+                        db.rollback()
         except Exception as e:
             print(f"Error in process_images: {e}")
             db.rollback()
         finally:
             db.close()
+
         time.sleep(1)
+
 
 def start_background_task():
     thread = Thread(target=process_images)
