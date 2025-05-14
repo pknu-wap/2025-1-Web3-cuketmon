773e6880766e957dad2812115603c874f65fa154
jso <c50691464@gmail.com>
2025-05-14 20:51:57 +0900

    [feat] 전투화면- 대규모 수정. . . 챗박스와 hp바 import 및 전투로직 변경 반영

--- cuketmon/src/common/TextBox/BattleChatbox.js
+++ cuketmon/src/common/TextBox/BattleChatbox.js
@@ -1,26 +1,21 @@
 import React, { useState } from 'react';
-import './BattleChatbox.css';
 import SkillButton from './SkillButton';
-import SkillDetail from './SkillDetail';
 
-const BattleChatbox = ({ skills }) => {
-  const [selected, setSelected] = useState(0);
+const BattleChatbox = ({ skills, onUse, isTurnInProgress }) => {
+  const [selected, setSelected] = useState(null);
 
   return (
     <div className="battleChatbox">
-      <div className="leftPanel">
-        {skills.map((skill, index) => (
-          <SkillButton
-            key={index}
-            skill={skill}
-            selected={selected === index}
-            onClick={() => setSelected(index)}
-          />
-        ))}
-      </div>
-      <div className="rightPanel">
-        <SkillDetail skill={skills[selected]} />
-      </div>
+      {skills.map((skill, index) => (
+        <SkillButton
+          key={index}
+          skill={skill}
+          selected={selected === index}
+          onSelect={() => setSelected(index)}
+          onUse={() => onUse(index)}
+          disabled={isTurnInProgress || skill.currentPp <= 0}
+        />
+      ))}
     </div>
   );
 };
