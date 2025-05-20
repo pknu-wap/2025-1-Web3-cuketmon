ccebc239022e9f2fa76ec37751ce052391b1bcca
kangrae <kangrae0501@gmail.com>
2025-05-20 21:22:50 +0900

    [fix] skillName도 받아오도록 수정

--- BE/src/main/java/cuketmon/battle/dto/BattleDTO.java
+++ BE/src/main/java/cuketmon/battle/dto/BattleDTO.java
@@ -17,9 +17,14 @@ public class BattleDTO {
         private String trainerName;
         private MonsterDTO.MonsterBattleInfo monster;
         private String skillAnimation;
+        private String skillName;
 
         public void changeSkillAnimation(String newSkillAnimation) {
             this.skillAnimation = newSkillAnimation;
+        }
+
+        public void changeSkillName(String newSkillName) {
+            this.skillName = newSkillName;
         }
     }
 
