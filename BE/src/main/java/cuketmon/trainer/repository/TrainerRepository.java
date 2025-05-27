0f0912e16d70210cc188d2af538342a6c193d33a
kangrae <kangrae0501@gmail.com>
2025-05-27 14:02:19 +0900

    [fix] jpa query가 올바르도록 변경

--- BE/src/main/java/cuketmon/trainer/repository/TrainerRepository.java
+++ BE/src/main/java/cuketmon/trainer/repository/TrainerRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 
 @Repository
@@ -18,7 +19,8 @@ public interface TrainerRepository extends JpaRepository<Trainer, String> {
 
     int countByWinGreaterThan(int win);
 
-    @Modifying
+    @Transactional
+    @Modifying(clearAutomatically = true, flushAutomatically = true)
     @Query("UPDATE Trainer t SET t.refreshToken = :refreshToken WHERE t.name = :trainerName")
     void updateRefreshToken(@Param("trainerName") String trainerName,
                             @Param("refreshToken") String refreshToken);
