bb80bcbf5e7c00d7a9e1da7dc4ac2befa8be3d3c
kangrae <kangrae0501@gmail.com>
2025-12-29 01:04:26 +0900

    refactor: 사용하지 않는 코드 삭제

--- BE/src/main/java/cuketmon/battle/repository/BattleRepository.java
+++
@@ -1,8 +0,0 @@
-package cuketmon.battle.repository;
-
-import cuketmon.battle.entity.Battle;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BattleRepository extends JpaRepository<Battle, Integer> {
-
-}
